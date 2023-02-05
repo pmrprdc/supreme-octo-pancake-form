81880c41e108d1c670e05e194b1621a5df850496
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-04 21:13:46 -0500

    input form has an onchange handler and renders input value as an h1

--- formapp/src/App.js
+++ formapp/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+ const [inputName, setInputName] = useState("")
+
+  const changeHandler = (evt) =>{
+    setInputName(evt.target.value)
+  }
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {inputName && <h1>{inputName}</h1>}
+        <img  src={logo} className="App-logo" alt="logo" />
         <form>
-            <label htmlfor="email">
-              Email <input name="email" type="email" placeholder=""/>
-            </label>
-            <br/>
-            <label>
-              Name
-            <input name="name"type="text" placeholder="" />
-    
-            </label>
-
+            <input autoFocus onChange={changeHandler} type="text" placeholder="Enter Name"/>
 
         </form>
       
